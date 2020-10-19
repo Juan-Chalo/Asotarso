97dfae57e8a581a1126b1a0b628b5975c82709b3
Jhon-Ch <jchalo1997@gmail.com>
2020-10-18 22:58:59 -0600

    arreglo formulario de contacto prueba 2

--- contact.php
+++ contact.php
@@ -83,6 +83,24 @@
        <!--<div>
           <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
         </div> -->
+        <?php
+          include ("controladores/comentariocontroller.php");
+          $comentario= new Comentario();
+          if(isset($_POST) && !empty($_POST)){
+          $nombre = $comentario->sanitize($_POST['nombre']);
+          $correo = $comentario->sanitize($_POST['correo']);
+          $asunto = $comentario->sanitize($_POST['asunto']);
+          $mensaje = $comentario->sanitize($_POST['mensaje']);
+
+          $res = $comentario->insertarComentario($nombre,$correo,$asunto,$mensaje);
+          if($res){
+          echo "<div class='alert alert-success' role='alert'>Mensaje Enviado! Gracias por Escribirnos! Nos comunicaremos lo mas pronto posible, Dios le bendiga!</div>";
+          }else{
+          echo "<div class='alert alert-danger' role='alert'>Error al Enviar el Mensaje :(</div>";
+          }
+
+          }
+        ?>
 
         <div class="row mt-5">
 
