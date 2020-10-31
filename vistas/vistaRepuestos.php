37c6980224c91ea803bc51b2b1b3a9ebe11f4399
Jhon-Ch <jchalo1997@gmail.com>
2020-10-30 19:22:01 -0600

    cambio en muestra de repuestos

--- vistas/vistaRepuestos.php
+++ vistas/vistaRepuestos.php
@@ -1,51 +1,81 @@
 <?php require_once "parteSuperiorSocio.php"?>
  <section class="content">
-   <div class="container-fluid">
-      <div class="block-header">
-          <h2>LISTA DE REPUESTOS DISPONIBLES</h2>
-      </div>
-      <!-- Basic Card -->
-      <div class="row clearfix">
 
-            <?php
-          	include ('../Controladores/clasesControladores.php');
-          	$repuestos = new Producto();
-          	$listado=$repuestos->leerProducto();
+<!-- Exportable Table -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Lista de Repuestos Activos /REPUESTOS ASOTARSO -CHIQUIMULILLA.
+                            </h2>
 
-            while ($row=mysqli_fetch_object($listado)){
-            $nombreProducto=$row->nombreProducto;
-            $precioProducto=$row->precioProducto;
-            $codigo=$row->codigo;
-            $existencia=$row->Existencia;
-            $Categoria=$row->CategoriaProducto_idCategoriaProducto;
-           ?>
-          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-              <div class="card">
+                        </div>
 
-                  <div class="header">
-                      <h1>
-                          <?php echo $nombreProducto;?>
-                      </h1>
-                  </div>
-                  <div class="body">
-                    <h3>Codigo:</h3>
-                      <?php echo $codigo;?>
-                      <h3>Precio:</h3>
-                        <?php echo $precioProducto;?>
-                        <h3>Categoria del Producto:</h3>
-                          <?php echo $Categoria;?>
-                          <h3>En Existencia:</h3>
-                            <?php echo $existencia;?>
-                  </div>
-              </div>
-          </div>
-          <?php
-          }
-        ?>
-      </div>
-      <!-- #END# Basic Card -->
-  </div>
+                        <div class="body">
+                        	<h4>Exportar en:</h4>
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre del Repuesto</th>
+                                            <th>Codigo del Producto</th>
+                                            <th>Precio del Producto</th>
+                                            <th>Categoria</th>
+                                            <th>En Existencia</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <<th>Nombre del Repuesto</th>
+                                            <th>Codigo del Producto</th>
+                                            <th>Precio del Producto</th>
+                                            <th>Categoria</th>
+                                            <th>En Existencia</th>
+                                        </tr>
+                                    </tfoot>
+
+                                    	<?php
+                                      include ('../Controladores/clasesControladores.php');
+                                    	$repuestos = new Producto();
+                                    	$listado=$repuestos->leerProducto();
+											?>
 
 
- </section>
- <?php require_once "parteInferiorSocio.php"?>
+                                    <tbody>
+
+                                    	<?php
+                                      while ($row=mysqli_fetch_object($listado)){
+                                      $nombreProducto=$row->nombreProducto;
+                                      $precioProducto=$row->precioProducto;
+                                      $codigo=$row->codigo;
+                                      $existencia=$row->Existencia;
+                                      $Categoria=$row->CategoriaProducto_idCategoriaProducto;
+									?>
+                                        <tr>
+                                          <td><?php echo $nombreProducto;?></td>
+                                          <td><?php echo $codigo;?></td>
+                                          <td><?php echo $precioProducto;?></td>
+                                          <td><?php echo $Categoria;?></td>
+                                          <td><?php echo $existencia; ?></td>
+
+                                        </tr>
+                                        <?php
+												}
+											?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Exportable Table -->
+
+
+
+
+
+</section>
+<?php require_once "parteInferiorSocio.php"?>
