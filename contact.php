0657a13d345f4ba4cb97fbc4a1bf8c9118739f59
Jhon-Ch <jchalo1997@gmail.com>
2020-11-04 17:15:28 -0600

    implementacion RECAPTCHA prueba 4

--- contact.php
+++ contact.php
@@ -120,7 +120,7 @@
                 if ($recaptcha != '') {
                   $secret = "6LfLHt8ZAAAAAOpPaY3u-jVEe9GRsihLKEf_yr9_";
                   $ip = $_SERVER['REMOTE_ADDR'];
-                  $var = file_get_content("https://www.google.com/recaptcha/api/siteverify?secrect=$secret&response=$recaptcha&remoteip=$ip");
+                  $var = file_get_content("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$recaptcha&remoteip=$ip");
                   $array = json_decode($var, true);
 
                   if ($array['success']) {
